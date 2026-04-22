d268039dd52b5d9a71a5f179d4319bc170178e76
Jun Wang <pon6543211@gmail.com>
2026-04-22 09:14:12 +0800

    Create 19-RemoveNthNodeFromEndofList

---
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.ts
@@ -0,0 +1,27 @@
+// Last updated: 2026/4/22 上午9:14:13
+1/**
+2 * Definition for singly-linked list.
+3 * class ListNode {
+4 *     val: number
+5 *     next: ListNode | null
+6 *     constructor(val?: number, next?: ListNode | null) {
+7 *         this.val = (val===undefined ? 0 : val)
+8 *         this.next = (next===undefined ? null : next)
+9 *     }
+10 * }
+11 */
+12
+13function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+14    let dummy = new ListNode(-1,head)
+15    let fast = dummy
+16    let slow = dummy
+17    for(let i = 0 ; i<n ; i++){
+18        fast = fast.next
+19    }
+20    while (fast.next !== null){
+21        fast = fast.next
+22        slow = slow.next
+23    }
+24    slow.next = slow.next.next
+25    return dummy.next
+26}
