f99a8f0cde9038bc641cb0b83c6a9c18779cd2b3
Jun Wang <pon6543211@gmail.com>
2026-05-02 17:13:57 +0800

    Create 75-SortColors

---
+++ 75-SortColors/75-SortColors.js
@@ -0,0 +1,9 @@
+// Last updated: 2026/5/2 下午5:13:57
+1/**
+2 * @param {number[]} nums
+3 * @return {void} Do not return anything, modify nums in-place instead.
+4 */
+5var sortColors = function(nums) {
+6    nums.sort((a,b)=>a-b)
+7    
+8};
