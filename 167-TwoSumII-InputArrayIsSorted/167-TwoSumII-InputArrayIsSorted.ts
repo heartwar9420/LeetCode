d14111f79598289309a1eb9a4900d96eac31bb5d
Jun Wang <pon6543211@gmail.com>
2026-05-06 17:36:46 +0800

    File updated at 2026/5/6 下午5:36:47

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.ts
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.ts
@@ -1,18 +1,18 @@
-// Last updated: 2026/4/22 上午11:50:24
+// Last updated: 2026/5/6 下午5:36:47
 1function twoSum(numbers: number[], target: number): number[] {
-2    let left = 0, right = numbers.length - 1
-3    while (left < right) {
-4        let sum = numbers[left] + numbers[right]
-5        if (sum === target) {
-6            return [left + 1, right + 1]
-7        } else if (sum < target) {
-8            left++
-9            
-10        } else if (sum > target) {
-11            right--
-12        }
-13    }
-14    return null
-15
+2    let left = 0, right = numbers.length-1
+3    while(left<right){
+4        let sum = numbers[left]+numbers[right]
+5        if(sum === target){
+6            return [left+1,right+1]
+7        }
+8        if (sum < target){
+9            left++
+10        }
+11        if(sum >target){
+12            right--
+13        }
+14    }
+15    return [-1,-1]
 16
 17};
