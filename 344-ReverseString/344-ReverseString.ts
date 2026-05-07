06f53109ae6ca9cefb1e482c964d289cb013571d
Jun Wang <pon6543211@gmail.com>
2026-05-07 14:14:55 +0800

    File updated at 2026/5/7 下午2:14:56

--- 344-ReverseString/344-ReverseString.ts
+++ 344-ReverseString/344-ReverseString.ts
@@ -1,16 +1,13 @@
-// Last updated: 2026/4/22 上午11:16:16
+// Last updated: 2026/5/7 下午2:14:56
 1/**
 2 Do not return anything, modify s in-place instead.
 3 */
 4function reverseString(s: string[]): void {
-5    let left = 0
-6    let right = s.length -1
-7    while (left<right){
-8        let temp = s[right]
-9        s[right] = s[left]
-10        s[left] = temp
-11        left ++
-12        right --
-13    }
-14
-15};
+5    let left = 0 , right = s.length-1
+6    while (left < right){
+7        [s[left],s[right]]=[s[right],s[left]]
+8        left++
+9        right--
+10    }
+11
+12};
