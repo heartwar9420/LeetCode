306e7e0cdff917df7b142e7d898519f9058293ec
Jun Wang <pon6543211@gmail.com>
2026-05-07 15:39:55 +0800

    File updated at 2026/5/7 下午3:39:56

--- 373-FindKPairswithSmallestSums/373-FindKPairswithSmallestSums.ts
+++ 373-FindKPairswithSmallestSums/373-FindKPairswithSmallestSums.ts
@@ -1,34 +1,33 @@
-// Last updated: 2026/5/7 上午9:21:09
+// Last updated: 2026/5/7 下午3:39:56
 1interface MealPair{
-2    chefVal : number;
-3    sideVal : number;
-4    sideIndex: number;
+2    chefVal:number;
+3    sideVal:number;
+4    sideIndex:number;
 5}
-6
-7function kSmallestPairs(nums1: number[], nums2: number[], k: number): number[][] {
-8    let pq = new PriorityQueue ((a:MealPair,b:MealPair)=>{
-9        return (a.chefVal+a.sideVal)-(b.chefVal+b.sideVal)
-10    })
-11    for ( let i = 0 ; i < Math.min(k,nums1.length); i++){
-12        pq.enqueue({
-13            chefVal : nums1[i],
-14            sideVal : nums2[0],
-15            sideIndex : 0
-16        })
-17    }
-18    let res = []
-19    while(k>0 && !pq.isEmpty()){
-20        let cur = pq.dequeue();
-21        k--;
-22        let nextIndex = cur.sideIndex+1;
-23        if(nextIndex < nums2.length){
-24            pq.enqueue({
-25                chefVal:cur.chefVal,
-26                sideVal:nums2[nextIndex],
-27                sideIndex:nextIndex
-28            })
-29        }
-30        res.push([cur.chefVal,cur.sideVal]);
-31    }
-32    return res
-33};
+6function kSmallestPairs(nums1: number[], nums2: number[], k: number): number[][] {
+7    let pq = new PriorityQueue((a:MealPair,b:MealPair)=>{
+8        return (a.chefVal+a.sideVal)-(b.chefVal+b.sideVal)
+9    })
+10    for (let i = 0 ; i < Math.min(k , nums1.length) ; i++){
+11        pq.enqueue({
+12            chefVal:nums1[i],
+13            sideVal:nums2[0],
+14            sideIndex:0
+15        })
+16    }
+17    let res = []
+18    while (k>0 && !pq.isEmpty()){
+19        let cur = pq.dequeue()
+20        let nextIndex = cur.sideIndex +1
+21        if(nextIndex<nums2.length){
+22            pq.enqueue({
+23                chefVal : cur.chefVal,
+24                sideVal : nums2[nextIndex],
+25                sideIndex : nextIndex
+26            })
+27        }
+28        res.push([cur.chefVal,cur.sideVal])
+29        k--
+30    }
+31    return res
+32};
