88d03f3864fa779f193c2562da42178116f070c5
Jun Wang <pon6543211@gmail.com>
2026-05-07 14:08:01 +0800

    File updated at 2026/5/7 下午2:08:02

--- 283-MoveZeroes/283-MoveZeroes.ts
+++ 283-MoveZeroes/283-MoveZeroes.ts
@@ -1,15 +1,13 @@
-// Last updated: 2026/4/22 上午11:11:09
+// Last updated: 2026/5/7 下午2:08:02
 1/**
 2 Do not return anything, modify nums in-place instead.
 3 */
 4function moveZeroes(nums: number[]): void {
-5    let slow = 0
-6    for (let i = 0 ; i < nums.length ; i++){
-7        if(nums[i]!==0){
-8            let temp = nums[slow]
-9            nums[slow] = nums[i]
-10            nums[i] = temp
-11            slow ++
-12        }
-13    }
-14};
+5    let slow = 0 
+6    for (let fast = 0;fast<nums.length;fast++){
+7        if(nums[fast]!==0){
+8            [nums[slow], nums[fast]] = [nums[fast], nums[slow]];
+9            slow ++
+10        }
+11    }
+12};
