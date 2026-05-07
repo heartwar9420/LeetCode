831ec8ee1ceedcf9e451c992127c1cc9381db19f
Jun Wang <pon6543211@gmail.com>
2026-05-07 13:42:18 +0800

    File updated at 2026/5/7 下午1:42:18

--- 27-RemoveElement/27-RemoveElement.ts
+++ 27-RemoveElement/27-RemoveElement.ts
@@ -1,15 +1,12 @@
-// Last updated: 2026/4/22 上午10:36:35
+// Last updated: 2026/5/7 下午1:42:18
 1function removeElement(nums: number[], val: number): number {
-2    let slow = 0 , fast = 0
-3    for (let i = 0 ; i<nums.length ; i++){
+2    let slow = 0 , fast = 0 
+3    while (fast<nums.length){
 4        if(nums[fast]!==val){
-5            nums[slow] = nums[fast]
+5            nums[slow]=nums[fast]
 6            slow++
-7        }else{
-8            nums[fast]=null
-9        }
-10        fast++
-11    }
-12    return slow
-13
-14};
+7        }
+8            fast++
+9    }
+10    return slow
+11};
