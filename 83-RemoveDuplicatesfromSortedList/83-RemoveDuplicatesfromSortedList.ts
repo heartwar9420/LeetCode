cf95a4bf384a34f8ac7f136ec6e79a70471f7ace
Jun Wang <pon6543211@gmail.com>
2026-05-07 12:07:44 +0800

    File updated at 2026/5/7 下午12:07:45

--- 83-RemoveDuplicatesfromSortedList/83-RemoveDuplicatesfromSortedList.ts
+++ 83-RemoveDuplicatesfromSortedList/83-RemoveDuplicatesfromSortedList.ts
@@ -1,4 +1,4 @@
-// Last updated: 2026/5/7 下午12:01:50
+// Last updated: 2026/5/7 下午12:07:45
 1/**
 2 * Definition for singly-linked list.
 3 * class ListNode {
@@ -12,13 +12,13 @@
 11 */
 12
 13function deleteDuplicates(head: ListNode | null): ListNode | null {
-14    let p = head
-15    while (p&&p.next){
-16        if(p.val===p.next.val){
-17            p.next = p.next.next
-18        }else{
-19            p = p.next
-20        }
-21    }
-22    return head
+14    let p = head
+15    while (p&&p.next){
+16        if(p.val===p.next.val){
+17            p.next = p.next.next
+18        }else{
+19            p = p.next
+20        }
+21    }
+22    return head
 23};
