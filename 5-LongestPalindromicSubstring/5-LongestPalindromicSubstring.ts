53a2c43c5ae21bbf094c78aba2a18fa06f857855
Jun Wang <pon6543211@gmail.com>
2026-05-08 16:03:47 +0800

    File updated at 2026/5/8 下午4:03:48

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.ts
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.ts
@@ -1,19 +1,18 @@
-// Last updated: 2026/5/7 下午3:13:58
+// Last updated: 2026/5/8 下午4:03:48
 1function longestPalindrome(s: string): string {
-2    let res = '';
-3    for (let i = 0 ; i <s.length ; i++){
-4        let s1 = palindrome(s,i,i);
-5        let s2 = palindrome(s,i,i+1);
-6        res = res.length > s1.length ? res : s1;
-7        res = res.length > s2.length ? res : s2
+2    const getPalindrome = (s:string ,l:number,r:number)=>{
+3        while (s[l]===s[r] && l>=0 && r<s.length){
+4            l--
+5            r++
+6        }
+7        return s.substring(l+1 , r);
 8    }
-9    return res
-10};
-11
-12function palindrome(s:string,l:number,r:number){
-13    while(l>=0 && r<s.length && s[l]===s[r]){
-14        l--
-15        r++
-16    }
-17    return s.substring(l+1 , r)
-18}
+9    let res = ''
+10    for (let i = 0 ; i<s.length ; i++){
+11        let s1 = getPalindrome(s,i,i)
+12        let s2 = getPalindrome(s,i,i+1)
+13        res = res.length > s1.length ? res : s1
+14        res = res.length > s2.length ? res : s2
+15    }
+16    return res
+17};
