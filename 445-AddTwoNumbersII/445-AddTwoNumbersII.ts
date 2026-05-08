0c89a6a2d01d96fc947bcb7ba7f98ddac5f1c40d
Jun Wang <pon6543211@gmail.com>
2026-05-08 16:17:57 +0800

    File updated at 2026/5/8 下午4:17:58

--- 445-AddTwoNumbersII/445-AddTwoNumbersII.ts
+++ 445-AddTwoNumbersII/445-AddTwoNumbersII.ts
@@ -1,4 +1,4 @@
-// Last updated: 2026/5/7 下午4:22:47
+// Last updated: 2026/5/8 下午4:17:58
 1/**
 2 * Definition for singly-linked list.
 3 * class ListNode {
@@ -12,34 +12,31 @@
 11 */
 12
 13function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-14    let stack1 = [] , stack2 = []
-15    let cur1 = l1 , cur2 = l2
-16    while(cur1){
-17        stack1.push(cur1.val)
-18        cur1 = cur1.next
-19    }
-20    while(cur2){
-21        stack2.push(cur2.val)
-22        cur2 = cur2.next
+14    let stack1 = []
+15    while(l1){
+16        stack1.push(l1.val)
+17        l1 = l1.next
+18    }
+19    let stack2 = []
+20    while(l2){
+21        stack2.push(l2.val)
+22        l2 = l2.next
 23    }
-24    let carry = 0
-25    let ans = null
-26    while(stack1.length!==0||stack2.length!==0||carry){
-27        let p1 = stack1.pop()
-28        if(!p1){
-29            p1 = 0
+24    let dummy = new ListNode(-1)
+25    let carry = 0
+26    while (stack1.length>0 || stack2.length>0 || carry>0){
+27        let val = carry
+28        if(stack1.length>0){
+29            val += stack1.pop()
 30        }
-31        let p2 = stack2.pop()
-32        if(!p2){
-33            p2 = 0
-34        }
-35        let val = carry
-36        val = carry+p1+p2
-37        carry = Math.floor(val/10);
-38        val = val%10
-39        let current = new ListNode(val);
-40        current.next = ans
-41        ans = current
-42    }
-43    return ans
-44};
+31        if(stack2.length>0){
+32            val += stack2.pop()
+33        }
+34        carry = Math.floor(val/10)
+35        val = val%10
+36        let newNode = new ListNode(val)
+37        newNode.next = dummy.next
+38        dummy.next = newNode
+39    }
+40    return dummy.next
+41};
