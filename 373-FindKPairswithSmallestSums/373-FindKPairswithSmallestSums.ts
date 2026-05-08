5c1bc5e8074f4d3275dce7f06806788bb807e071
Jun Wang <pon6543211@gmail.com>
2026-05-08 15:46:41 +0800

    File updated at 2026/5/8 下午3:46:41

--- 373-FindKPairswithSmallestSums/373-FindKPairswithSmallestSums.ts
+++ 373-FindKPairswithSmallestSums/373-FindKPairswithSmallestSums.ts
@@ -1,33 +1,33 @@
-// Last updated: 2026/5/7 下午3:39:56
+// Last updated: 2026/5/8 下午3:46:41
 1interface MealPair{
-2    chefVal:number;
-3    sideVal:number;
-4    sideIndex:number;
+2    chefVal : number;
+3    sideVal : number;
+4    sideIndex : number;
 5}
 6function kSmallestPairs(nums1: number[], nums2: number[], k: number): number[][] {
 7    let pq = new PriorityQueue((a:MealPair,b:MealPair)=>{
-8        return (a.chefVal+a.sideVal)-(b.chefVal+b.sideVal)
+8        return (a.chefVal+a.sideVal) - (b.chefVal+b.sideVal)
 9    })
-10    for (let i = 0 ; i < Math.min(k , nums1.length) ; i++){
+10    for (let i = 0 ; i<Math.min(k , nums1.length) ; i++){
 11        pq.enqueue({
-12            chefVal:nums1[i],
-13            sideVal:nums2[0],
-14            sideIndex:0
+12            chefVal : nums1[i],
+13            sideVal : nums2[0],
+14            sideIndex : 0
 15        })
 16    }
 17    let res = []
-18    while (k>0 && !pq.isEmpty()){
-19        let cur = pq.dequeue()
-20        let nextIndex = cur.sideIndex +1
-21        if(nextIndex<nums2.length){
-22            pq.enqueue({
-23                chefVal : cur.chefVal,
-24                sideVal : nums2[nextIndex],
-25                sideIndex : nextIndex
-26            })
-27        }
-28        res.push([cur.chefVal,cur.sideVal])
-29        k--
+18    while(k>0 && !pq.isEmpty()){
+19        k--
+20        let cur = pq.dequeue()
+21        res.push([cur.chefVal,cur.sideVal])
+22        let nextIndex = cur.sideIndex+1
+23        if(nextIndex<nums2.length){
+24            pq.enqueue({
+25                chefVal:cur.chefVal,
+26                sideVal:nums2[nextIndex],
+27                sideIndex:nextIndex
+28            })
+29        }
 30    }
 31    return res
 32};
