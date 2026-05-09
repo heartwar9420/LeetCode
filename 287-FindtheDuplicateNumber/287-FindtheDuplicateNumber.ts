273fb89d2586d252dfb5253463ad8c96917e218b
Jun Wang <pon6543211@gmail.com>
2026-05-09 10:52:50 +0800

    File updated at 2026/5/9 上午10:52:50

--- 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.ts
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.ts
@@ -1,15 +1,16 @@
-// Last updated: 2026/5/8 下午5:29:07
+// Last updated: 2026/5/9 上午10:52:50
 1function findDuplicate(nums: number[]): number {
-2    let slow = 0 , fast = 0
-3    while(true){
-4        fast = nums[nums[fast]]
+2    let slow = 0
+3    let fast = 0
+4    while(true){
 5        slow = nums[slow]
-6        if(slow===fast) break
-7    }
-8    slow = 0
-9    while(slow!==fast){
-10        fast = nums[fast]
+6        fast = nums[nums[fast]]
+7        if(slow===fast)break
+8    }
+9    slow = 0
+10    while(slow!==fast){
 11        slow = nums[slow]
-12    }
-13    return slow
-14};
+12        fast = nums[fast]
+13    }
+14    return slow
+15};
