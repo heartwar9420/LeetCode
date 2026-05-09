aba4357e0e010cef9855c77f91154f6c6c735aa3
Jun Wang <pon6543211@gmail.com>
2026-05-09 09:29:52 +0800

    File updated at 2026/5/9 上午9:29:53

--- 373-FindKPairswithSmallestSums/373-FindKPairswithSmallestSums.ts
+++ 373-FindKPairswithSmallestSums/373-FindKPairswithSmallestSums.ts
@@ -1,22 +1,22 @@
-// Last updated: 2026/5/8 下午3:46:41
+// Last updated: 2026/5/9 上午9:29:53
 1interface MealPair{
-2    chefVal : number;
-3    sideVal : number;
-4    sideIndex : number;
+2    chefVal:number
+3    sideVal:number
+4    sideIndex:number
 5}
 6function kSmallestPairs(nums1: number[], nums2: number[], k: number): number[][] {
 7    let pq = new PriorityQueue((a:MealPair,b:MealPair)=>{
-8        return (a.chefVal+a.sideVal) - (b.chefVal+b.sideVal)
+8        return (a.chefVal+a.sideVal)-(b.chefVal+b.sideVal)
 9    })
-10    for (let i = 0 ; i<Math.min(k , nums1.length) ; i++){
+10    for(let i = 0 ; i<Math.min(k,nums1.length);i++){
 11        pq.enqueue({
-12            chefVal : nums1[i],
-13            sideVal : nums2[0],
-14            sideIndex : 0
+12            chefVal:nums1[i],
+13            sideVal:nums2[0],
+14            sideIndex:0,
 15        })
 16    }
 17    let res = []
-18    while(k>0 && !pq.isEmpty()){
+18    while(k>0&&!pq.isEmpty()){
 19        k--
 20        let cur = pq.dequeue()
 21        res.push([cur.chefVal,cur.sideVal])
