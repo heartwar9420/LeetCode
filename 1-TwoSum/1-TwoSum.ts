e558f06f2c4b529986a4fbacddea10e1c2f89f80
Jun Wang <pon6543211@gmail.com>
2026-05-10 09:46:51 +0800

    File updated at 2026/5/10 上午9:46:51

--- 1-TwoSum/1-TwoSum.ts
+++ 1-TwoSum/1-TwoSum.ts
@@ -1,11 +1,12 @@
-// Last updated: 2026/5/9 上午11:51:25
+// Last updated: 2026/5/10 上午9:46:51
 1function twoSum(nums: number[], target: number): number[] {
-2    const map = new Map()
-3    for(let i = 0 ; i < nums.length ; i++){
-4        const complete = (target-nums[i])
+2    let map = new Map()
+3    for(let i = 0;i<nums.length;i++){
+4        let complete = target - nums[i]
 5        if(map.has(complete)){
-6            return [map.get(complete),i]
+6            return[map.get(complete),i]
 7        }
 8        map.set(nums[i],i)
 9    }
-10};
+10    return null
+11};
