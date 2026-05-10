bad96c0aaf9809499aca074586f0252b45c2fb11
Jun Wang <pon6543211@gmail.com>
2026-05-10 10:15:54 +0800

    File updated at 2026/5/10 上午10:15:54

--- 15-3Sum/15-3Sum.ts
+++ 15-3Sum/15-3Sum.ts
@@ -1,23 +1,23 @@
-// Last updated: 2026/5/9 下午1:56:50
+// Last updated: 2026/5/10 上午10:15:54
 1function threeSum(nums: number[]): number[][] {
 2    nums.sort((a,b)=>a-b)
-3    const res : number[][] = []
-4    for(let i = 0 ; i<nums.length-2;i++){
-5        if(i>0 && nums[i]===nums[i-1]) continue
-6        let lo = i+1;
-7        let hi = nums.length-1  
+3    let res :number[][] = []
+4    for (let i = 0 ; i < nums.length-2 ; i++){
+5        if( i>0 && nums[i]===nums[i-1]) continue
+6        let lo = i+1
+7        let hi = nums.length-1
 8        while(lo<hi){
-9            let sum = nums[i]+nums[lo]+nums[hi]
-10            if(sum < 0){
-11                lo++
-12            }else if (sum >0){
-13                hi--
-14            }else{
-15                res.push([nums[i],nums[lo],nums[hi]])
-16                while(lo<hi && nums[lo]===nums[lo+1]) lo++
-17                while(lo<hi && nums[hi]===nums[hi-1]) hi--;
-18                lo++
-19                hi--
+9            let target = nums[i]+nums[lo]+nums[hi]
+10            if(target===0){
+11                res.push([nums[i],nums[lo],nums[hi]])
+12                while(lo<hi && nums[lo]===nums[lo+1]) lo++
+13                while(lo<hi && nums[hi]===nums[hi-1]) hi--
+14                lo++
+15                hi--
+16            } else if (target<0){
+17                lo++
+18            }else {
+19            hi--
 20            }
 21        }
 22    }
