87a5362fc9d6f093256e2128de7a6c17fd45a95f
Jun Wang <pon6543211@gmail.com>
2026-05-10 09:39:56 +0800

    File updated at 2026/5/10 上午9:39:57

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.ts
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.ts
@@ -1,14 +1,14 @@
-// Last updated: 2026/5/9 下午5:30:04
+// Last updated: 2026/5/10 上午9:39:57
 1function maxArea(height: number[]): number {
-2    let left = 0 , right = height.length-1 , res = 0
-3    while(left<right){
-4        let curArea = (Math.min(height[left],height[right]))*(right-left)
-5        res = Math.max(res,curArea)
-6        if(height[left]<height[right]){
-7            left++
-8        }else{
-9            right--
-10        }
+2    let left = 0
+3    let right = height.length-1
+4    let res = 0
+5    while(left<right){
+6        let curArea = Math.min(height[left],height[right]) * (right-left)
+7        res = Math.max(res,curArea)
+8        if(height[left]<height[right]){
+9            left++
+10        }else right --
 11    }
 12    return res
 13};
