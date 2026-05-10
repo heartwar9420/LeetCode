42c7aeac1b495630a95e04b5992fc1876d1192d2
Jun Wang <pon6543211@gmail.com>
2026-05-10 11:06:14 +0800

    File updated at 2026/5/10 上午11:06:15

--- 445-AddTwoNumbersII/445-AddTwoNumbersII.ts
+++ 445-AddTwoNumbersII/445-AddTwoNumbersII.ts
@@ -1,4 +1,4 @@
-// Last updated: 2026/5/9 上午10:17:01
+// Last updated: 2026/5/10 上午11:06:15
 1/**
 2 * Definition for singly-linked list.
 3 * class ListNode {
@@ -13,24 +13,24 @@
 12
 13function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
 14    let dummy = new ListNode(-1)
-15    let stack1:number[] = []
+15    let stk1=[]
 16    while(l1){
-17        stack1.push(l1.val)
+17        stk1.push(l1.val)
 18        l1 = l1.next
 19    }
-20    let stack2:number[] = []
+20    let stk2=[]
 21    while(l2){
-22        stack2.push(l2.val)
+22        stk2.push(l2.val)
 23        l2 = l2.next
 24    }
 25    let carry = 0
-26    while(stack1.length>0 || stack2.length>0 || carry>0){
-27        let val = carry;
-28        if(stack1.length>0){
-29            val += stack1.pop()
+26    while(stk1.length>0 || stk2.length>0 || carry>0){
+27        let val = carry
+28        if(stk1.length>0){
+29            val += stk1.pop()
 30        }
-31        if(stack2.length>0){
-32            val += stack2.pop()
+31        if(stk2.length>0){
+32            val+= stk2.pop()
 33        }
 34        carry = Math.floor(val/10)
 35        val = val%10
