aa1cfefb4509dff3494785a40d070c1716153a50
Jun Wang <pon6543211@gmail.com>
2026-05-10 10:02:38 +0800

    File updated at 2026/5/10 上午10:02:38

--- 42-TrappingRainWater/42-TrappingRainWater.ts
+++ 42-TrappingRainWater/42-TrappingRainWater.ts
@@ -1,19 +1,16 @@
-// Last updated: 2026/5/9 下午5:07:09
+// Last updated: 2026/5/10 上午10:02:38
 1function trap(height: number[]): number {
-2    let left = 0 , right = height.length-1
-3    let lMax = 0 , rMax = 0
-4    let res = 0
-5    while (left<right){
-6        lMax = Math.max(lMax,height[left]);
-7        rMax = Math.max(rMax,height[right]);
-8
-9        if(lMax<rMax){
-10            res += lMax -height[left]
-11            left ++
-12        }else{
-13            res += rMax - height[right]
-14            right--
-15        }
-16    }
-17    return res
-18};
+2    let lMax = 0 , rMax = 0 , left = 0 , right = height.length-1 ,res = 0
+3    while(left<right){
+4        lMax = Math.max(lMax,height[left])
+5        rMax = Math.max(rMax,height[right])
+6        if(lMax<rMax){
+7            res += lMax-height[left]
+8            left++
+9        }else{
+10            res += rMax-height[right]
+11            right--
+12        }
+13    }
+14    return res
+15};
