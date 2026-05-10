dd517f78658127db0f07ccce0ac2d2d6d8c7b4f0
Jun Wang <pon6543211@gmail.com>
2026-05-10 11:47:48 +0800

    File updated at 2026/5/10 上午11:47:48

--- 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.ts
+++ 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.ts
@@ -1,19 +1,12 @@
-// Last updated: 2026/5/10 上午11:41:20
+// Last updated: 2026/5/10 上午11:47:48
 1function removeDuplicates(nums: number[]): number {
-2    let slow =0 , fast = 0 ,count = 0
-3    while(fast<nums.length){
-4        if(nums[fast]!==nums[slow]){
-5            slow++
-6            nums[slow]=nums[fast]
-7        }else if (slow < fast &&count<2){
-8            slow++
-9            nums[slow]=nums[fast]
-10        }
-11        fast++
-12        count++
-13        if(fast<nums.length &&nums[fast]!==nums[fast-1]){
-14            count = 0
-15        }
-16    }
-17    return slow +1
-18};
+2    let slow = 2
+3    for (let fast = 2 ; fast<nums.length ; fast++){
+4        if(nums[fast]!== nums[slow-2]){
+5            nums[slow]=nums[fast]
+6            slow ++
+7        }
+8    }
+9    return slow
+10
+11};
