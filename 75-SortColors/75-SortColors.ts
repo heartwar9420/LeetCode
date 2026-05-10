e885a50500a3615ac379d69fafc340c4f0d4a69d
Jun Wang <pon6543211@gmail.com>
2026-05-10 14:05:10 +0800

    Create 75-SortColors

---
+++ 75-SortColors/75-SortColors.ts
@@ -0,0 +1,7 @@
+// Last updated: 2026/5/10 下午2:05:10
+1/**
+2 Do not return anything, modify nums in-place instead.
+3 */
+4function sortColors(nums: number[]): void {
+5    nums.sort((a,b)=>a-b)
+6};
